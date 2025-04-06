efdcf5d884030cefb057c026c085a863a37372c1
Scott Zeta <scott-zeta@outlook.com>
2025-04-06 14:12:13 +0930

    Add the logic behind the game

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from 'react';
 
 export default function GamePage() {
   const [shuffledValues, setShuffledValues] = useState<Value[]>([]);
+  const [randomThreeValues, setRandomThreeValues] = useState<Value[]>([]);
+
   const shuffleArray = (array: Value[]) => {
     const arr = [...array];
     for (let i = arr.length - 1; i > 0; i--) {
@@ -14,12 +16,69 @@ export default function GamePage() {
     return arr;
   };
 
+  const getRandomThreeValues = (pool: Value[]) => {
+    const indices = new Set<number>();
+    while (indices.size < 3 && indices.size < pool.length) {
+      indices.add(Math.floor(Math.random() * pool.length));
+    }
+    return [...indices].map((index) => pool[index]);
+  };
+
+  const removeValueFromList = (value: Value, pool: Value[]) => {
+    return pool.filter((v) => v !== value);
+  };
+
   useEffect(() => {
     setShuffledValues(shuffleArray(valueList));
+    setRandomThreeValues(getRandomThreeValues(valueList));
   }, []);
+
   useEffect(() => {
     console.log(shuffledValues);
-  }, [shuffledValues]);
+    console.log(randomThreeValues);
+  }, [shuffledValues, randomThreeValues]);
 
-  return <div>Game</div>;
+  return (
+    <>
+      {shuffledValues.length > 5 ? (
+        <>
+          <p>Pick a value you think is LEAST Important</p>
+          {randomThreeValues.map((value: Value) => {
+            return (
+              <div
+                key={value.key}
+                onClick={() => {
+                  const updatedValues = removeValueFromList(
+                    value,
+                    shuffledValues
+                  );
+                  setShuffledValues(updatedValues);
+                  setRandomThreeValues(getRandomThreeValues(updatedValues));
+                }}
+                className="border-2 border-gray-300 rounded-lg p-4 m-2 cursor-pointer hover:bg-gray-100 transition duration-200 ease-in-out"
+              >
+                <h1>{value.name}</h1>
+                <p>{value.description}</p>
+              </div>
+            );
+          })}
+        </>
+      ) : (
+        <>
+          <p>Your Final 5 Most Important Value</p>
+          {shuffledValues.map((value: Value) => {
+            return (
+              <div
+                key={value.key}
+                className="border-2 border-gray-300 rounded-lg p-4 m-2 cursor-pointer hover:bg-gray-100 transition duration-200 ease-in-out"
+              >
+                <h1>{value.name}</h1>
+                <p>{value.description}</p>
+              </div>
+            );
+          })}
+        </>
+      )}
+    </>
+  );
 }
